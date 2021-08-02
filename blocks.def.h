73cb1012a961ac54643854538ff758e4e07a999a
Mišo Barišić <miki.barisic5@gmail.com>
2021-08-02 13:11:06 +0200

    dwmblocks files

---
+++ blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"  ", "/home/mb/.config/dwmblocks/memory_gb",	5,		0},
+	{"", "/home/mb/.config/dwmblocks/memory_percentage",	5,		0},
+	{"|  ", "/home/mb/.config/dwmblocks/cpu",	2,		0},
+	/*{" | ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
+	{"| ",     "/home/mb/.config/dwmblocks/kernel",	0,		0},
+	{"| ",     "/home/mb/.config/dwmblocks/clock",	15,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 5;
