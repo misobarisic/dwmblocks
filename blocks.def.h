6164cac3215cfd59789aaefa29c962a6a4dac609
Mišo Barišić <miki.barisic5@gmail.com>
2021-08-10 11:54:52 +0200

    blocks update

--- blocks.def.h
+++ blocks.def.h
@@ -3,10 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"  ", "/home/mb/.config/dwmblocks/memory_gb",	5,		0},
 	{"", "/home/mb/.config/dwmblocks/memory_percentage",	5,		0},
-	{"|  ", "/home/mb/.config/dwmblocks/cpu",	2,		0},
-	/*{" | ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
+	{"|  ", "/home/mb/.config/dwmblocks/cpu_percentage",	2,		0},
+	{"", "/home/mb/.config/dwmblocks/cpu_bars",	2,		0},
+	/*{"| ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
 	{"| ",     "/home/mb/.config/dwmblocks/kernel",	0,		0},
 	{"| ",     "/home/mb/.config/dwmblocks/clock",	15,		0},
+	{" ",     "",	                                 0,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
