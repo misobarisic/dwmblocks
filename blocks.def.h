0a2da5c56360849f5437336656bf85efc4e1dfa9
Mišo Barišić <miki.barisic5@gmail.com>
2021-08-21 11:47:41 +0200

    update

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "/home/mb/.config/dwmblocks/memory_gb",	5,		0},
+	{"  ", "/home/mb/.config/dwmblocks/memory_bytes",	5,		0},
 	{"", "/home/mb/.config/dwmblocks/memory_percentage",	5,		0},
 	{"|  ", "/home/mb/.config/dwmblocks/cpu_percentage",	2,		0},
 	{"", "/home/mb/.config/dwmblocks/cpu_bars",	2,		0},
+	{"|  ", "/home/mb/.config/dwmblocks/storage",	60,		0},
 	/*{"| ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
 	/*{"| ",     "/home/mb/.config/dwmblocks/kernel",	0,		0},*/
 	{"|  ",     "/home/mb/.config/dwmblocks/volume_l",	0,		10},
