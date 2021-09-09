6b82fc9c1b56a3f0ace015ab07ce40176850bb29
Mišo Barišić <miki.barisic5@gmail.com>
2021-09-09 12:54:25 +0200

    .config -> .scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "/home/mb/.config/dwmblocks/memory_bytes",	5,		0},
-	{"", "/home/mb/.config/dwmblocks/memory_percentage",	5,		0},
-	{"|  ", "/home/mb/.config/dwmblocks/cpu_percentage",	2,		0},
-	{"", "/home/mb/.config/dwmblocks/cpu_bars",	2,		0},
-	{"|  ", "/home/mb/.config/dwmblocks/storage",	60,		0},
-	/*{"| ",     "/home/mb/.config/dwmblocks/arch_upt",	120,		0},*/
-	/*{"| ",     "/home/mb/.config/dwmblocks/kernel",	0,		0},*/
-	{"|  ",     "/home/mb/.config/dwmblocks/volume_l",	0,		10},
-	{"| ",     "/home/mb/.config/dwmblocks/clock",	15,		0},
+	{"  ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
+	{"", "/home/mb/.scripts/dwmblocks/memory_percentage",	5,		0},
+	{"|  ", "/home/mb/.scripts/dwmblocks/cpu_percentage",	2,		0},
+	{"", "/home/mb/.scripts/dwmblocks/cpu_bars",	2,		0},
+//	{"|  ", "/home/mb/.scripts/dwmblocks/storage",	60,		0},
+//	{"| ",     "/home/mb/.scripts/dwmblocks/arch_upt",	120,		0},
+//	{"| ",     "/home/mb/.scripts/dwmblocks/kernel",	0,		0},
+	{"|  ",     "/home/mb/.scripts/dwmblocks/volume_l",	0,		10},
+	{"| ",     "/home/mb/.scripts/dwmblocks/clock",	15,		0},
 	{" ",     "",	                                 0,		0},
 };
 
