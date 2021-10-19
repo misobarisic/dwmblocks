d0863eb765223360a702dc60b729a4e2e56ab341
Mišo Barišić <miki.barisic5@gmail.com>
2021-10-19 16:53:35 +0200

    update

--- blocks.def.h
+++ blocks.def.h
@@ -2,18 +2,22 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"  ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
+//	{" M: ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
 	{"", "/home/mb/.scripts/dwmblocks/memory_percentage",	5,		0},
 	{"|  ", "/home/mb/.scripts/dwmblocks/cpu_percentage",	2,		0},
+//	{"| C: ", "/home/mb/.scripts/dwmblocks/cpu_percentage",	2,		0},
 //	{"", "/home/mb/.scripts/dwmblocks/cpu_bars",	2,		0},
-	{"| ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
+	{"|  ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
+//	{"| B: ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
 //	{"|  ", "/home/mb/.scripts/dwmblocks/storage",	60,		0},
 //	{"| ",     "/home/mb/.scripts/dwmblocks/arch_upt",	120,		0},
 //	{"| ",     "/home/mb/.scripts/dwmblocks/kernel",	0,		0},
 	{"|  ",     "/home/mb/.scripts/dwmblocks/volume_l",	0,		10},
+//	{"| V: ",     "/home/mb/.scripts/dwmblocks/volume_l",	0,		10},
 	{"| ",     "/home/mb/.scripts/dwmblocks/clock",	15,		0},
 	{" ",     "",	                                 0,		0},
 };
-
+//
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " ";
 static unsigned int delimLen = 5;
