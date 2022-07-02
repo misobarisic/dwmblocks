1afd66930d9cb4b3c0eb5ee8aecd2064414d0f6a
Mišo Barišić <me@misobarisic.com>
2022-07-02 18:06:00 +0200

    battery script
    Signed-off-by: Mišo Barišić <me@misobarisic.com>

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
+	{"  ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
 //	{" M: ", "/home/mb/.scripts/dwmblocks/memory_bytes",	5,		0},
 	{"", "/home/mb/.scripts/dwmblocks/memory_percentage",	5,		0},
 	{"|  ", "/home/mb/.scripts/dwmblocks/cpu_percentage",	2,		0},
 //	{"| C: ", "/home/mb/.scripts/dwmblocks/cpu_percentage",	2,		0},
 //	{"", "/home/mb/.scripts/dwmblocks/cpu_bars",	2,		0},
-	{"|  ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
+//	{"|  ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
+	{"| ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
 //	{"| B: ", "/home/mb/.scripts/dwmblocks/battery",	90,		0},
 //	{"|  ", "/home/mb/.scripts/dwmblocks/storage",	60,		0},
 //	{"| ",     "/home/mb/.scripts/dwmblocks/arch_upt",	120,		0},
